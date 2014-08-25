a9d7366a195cea99dbde2bef74967b0830d209e1
linzhuangxu <444905823@qq.com>
2014-08-26 00:35:12 +0800

    modified:   polls/models.py 	new file:   polls/static/polls/style.css 	new file:   polls/templates/polls/index.html 	modified:   polls/tests.py 	deleted:    templates/polls/index.html

--- webpy/polls/models.py
+++ webpy/polls/models.py
@@ -1,7 +1,11 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
 class  Poll(models.Model):
-	"""poll"""
+
+	def was_published_recently(self):
+		now = timezone.now()
+		return now - datetime.timedelta(days=1) <= self.pub_date <  now
 	
 
