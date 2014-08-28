8b1ef02e65a4eee817f5ceb04fdcd0a066f74591
linzhuangxu <444905823@qq.com>
2014-08-29 00:25:41 +0800

    new file:   userApp/form.py

---
+++ webpy/userApp/form.py
@@ -0,0 +1,13 @@
+from datetime import date
+from django.forms import widgets
+
+class DateSelectorWidget(widgets.MultiWidget):
+    def __init__(self, attrs=None):
+        # create choices for days, months, years
+        # example below, the rest snipped for brevity.
+        years = [(year, year) for year in (2011, 2012, 2013)]
+        _widgets = (
+            widgets.Select(attrs=attrs, choices=days),
+            widgets.Select(attrs=attrs, choices=months),
+            widgets.Select(attrs=attrs, choices=years),
+        )
